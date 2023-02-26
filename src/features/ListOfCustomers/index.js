a27c13d83839e7d9566270d00035544b461db96e
DonatellaC <donatella.carta@mailbox.org>
2023-02-26 20:28:28 +0100

    feat: added create new customer button in customers by region

--- src/features/ListOfCustomers/index.js
+++ src/features/ListOfCustomers/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { View, Text, FlatList } from "react-native";
-import { useRoute } from "@react-navigation/native";
+import { useRoute, useNavigation } from "@react-navigation/native";
 import stylesFn from "./styles";
 import ShowCustomerCard from "../../components/ShowCustomerCard";
+import { Button } from "../../components/Button/Button";
 
 const ListOfCustomers = () => {
   const route = useRoute();
+  const { navigate } = useNavigation();
   const styles = stylesFn();
   const { region, filteredCustomers } = route.params;
 
@@ -28,6 +30,10 @@ const ListOfCustomers = () => {
       ) : (
         <Text style={styles.noCustomers}>No customers in {region}</Text>
       )}
+      <Button
+        onPress={() => navigate("Create")}
+        text="Create New Customer"
+      ></Button>
     </View>
   );
 };
